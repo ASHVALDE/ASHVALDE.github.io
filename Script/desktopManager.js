b88a993cbb58ccdb4d366adcf8f07bda72c4f3be
SantiagoValderrama <69223535+ASHVALDE@users.noreply.github.com>
2023-12-11 12:26:01 -0500

    Eliminados los placeholders

--- Script/desktopManager.js
+++ Script/desktopManager.js
@@ -1,4 +1,4 @@
-var programs = ["Informacion","Proyectos","Historia Laboral","Sobre Mi"]
+var programs = ["Sobre Mi"]
 var icons  = document.getElementsByClassName("icon")
 var currentActive = null
 const startIcon = (e)=>{
