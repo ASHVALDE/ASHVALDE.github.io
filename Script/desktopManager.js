0fc66a81c56cec8c9f861ada92bfcd6eaa46310d
Santiago Valderrama Monroy <69223535+ASHVALDE@users.noreply.github.com>
2024-02-25 01:36:59 -0500

    Eliminada opcion de proyectos de mientras

--- Script/desktopManager.js
+++ Script/desktopManager.js
@@ -1,4 +1,4 @@
-var programs = ["Proyectos","Sobre Mi","Configuraciones"]
+var programs = ["Sobre Mi","Configuraciones"]
 var icons  = document.getElementsByClassName("icon")
 var currentActive = null
 const startIcon = (e)=>{
