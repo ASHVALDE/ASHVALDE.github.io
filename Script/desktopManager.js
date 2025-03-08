c358cd081cb765e717940c74355446c436d980e2
Santiago Valderrama Monroy <69223535+ASHVALDE@users.noreply.github.com>
2025-03-07 21:40:08 -0500

    Prueba Doom

--- Script/desktopManager.js
+++ Script/desktopManager.js
@@ -1,4 +1,4 @@
-var programs = ["Sobre Mi","Proyectos","Buscaminas","Configuraciones","Mario 64"]
+var programs = ["Sobre Mi","Proyectos","Buscaminas","Configuraciones","Mario 64","Doom"]
 var icons  = document.getElementsByClassName("icon")
 var currentActive = null
 const startIcon = (e)=>{
---
+++ apps/Doom/data.js
@@ -0,0 +1,4 @@
+window.addEventListener("keydown", (event) => {
+    const iframe = document.getElementById("Mario64");
+    iframe.focus();
+});
