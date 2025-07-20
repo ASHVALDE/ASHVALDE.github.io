8e62de69baf5e8c57eb7e7a29721a8563dc1d574
Santiago Valderrama Monroy <69223535+ASHVALDE@users.noreply.github.com>
2025-07-20 00:04:05 -0500

    HabboTest

--- Script/desktopManager.js
+++ Script/desktopManager.js
@@ -1,4 +1,4 @@
-var programs = ["Sobre Mi","Biblioteca","Proyectos","Musica","Configuraciones",]
+var programs = ["Sobre Mi","Biblioteca","Proyectos","Musica","Configuraciones","Habbo"]
 var icons  = document.getElementsByClassName("icon")
 var currentActive = null
 const startIcon = (e)=>{
