79f5c00abf916b7061f7444b42c566d34b2c902d
Santiago Valderrama Monroy <69223535+ASHVALDE@users.noreply.github.com>
2026-04-19 12:26:01 -0500

    agregada mas musica

--- apps/Winamp/data.js
+++ apps/Winamp/data.js
@@ -13,16 +13,42 @@ const webamp = new Webamp({
       },
       url: "public/music/2.mp3"
     },
-{
+    {
       metaData: {
         artist: "Evanescence",
         title: "Bring me 2 life"
       },
       url: "public/music/3.mp3"
     },
-
+    {
+      metaData: {
+        artist: "untitled",
+        title: "Broly Vs. Goku"
+      },
+      url: "public/music/4.mp3"
+    },
+    {
+      metaData: {
+        artist: "untitled",
+        title: "bonjovi - its_my_life_official_video"
+      },
+      url: "public/music/5.mp3"
+    },
+    {
+      metaData: {
+        artist: "Linkin Park",
+        title: "GivenUp"
+      },
+      url: "public/music/6.mp3"
+    },
+    {
+      metaData: {
+        artist: "Green Day",
+        title: "Boulevard of Broken Dreams (Official Audio)"
+      },
+      url: "public/music/7.mp3"
+    },
 ],
-
   });
 
   webamp.renderWhenReady(document.getElementById("winamp"));
